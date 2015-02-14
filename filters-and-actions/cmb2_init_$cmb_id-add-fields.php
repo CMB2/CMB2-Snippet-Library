441c211591093d2f47175ae69772a89152b97769
Justin Sternberg <justin@dsgnwrks.pro>
2015-02-14 15:24:48 -0500

    Add example using the cmb2_init_{$cmb_id} hook

---
+++ filters-and-actions/cmb2_init_$cmb_id-add-fields.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file demonstrates adding new fields to a registered CMB2 metaboxes during its "cmb2_init_{$cmb_id}" hook
+ */
+
+function yourprefix_add_new_field_to_top( $cmb ) {
+
+	$cmb->add_field(
+		array(
+			'name'       => __( 'New at the top', 'your_textdomain' ),
+			'desc'       => __( 'Using the "cmb2_init_{$cmb_id}" hook', 'your_textdomain' ),
+			'id'         => '_new_at_the_top',
+			'type'       => 'text',
+		),
+		'',
+		1
+	);
+
+}
+add_action( 'cmb2_init__yourprefix_demo_metabox', 'yourprefix_add_new_field_to_top' );
