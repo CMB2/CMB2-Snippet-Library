3ba227a19aeb5448cf9d5e285543c480deccea60
johnsonpaul1014 <pjohnson@tpt.org>
2015-02-15 16:11:04 -0600

    A few more tweaks

--- custom-field-types/autocomplete-field-type.php
+++ custom-field-types/autocomplete-field-type.php
@@ -18,7 +18,7 @@
  *							The example fields in this plugin demonstrate all types: pre-built single field, pre-built repeatable field, remote single field
  *							and remote repeatable field.
  * Author: johnsonpaul1014
- * Version: 0.1.0
+ * Version: 1.0.0
  */
 
 /**
@@ -117,7 +117,10 @@ function autocomplete_cmb2_render_autocomplete($field_object, $escaped_value, $o
 	// Store the value in a hidden field.
 	echo $field_type_object->hidden();
 
-	$repeatable_class = $field_object->args['repeatable_class'];
+	if (isset($field_object->args['repeatable_class'])) {
+		$repeatable_class = $field_object->args['repeatable_class'];
+	}
+
 	$options = $field_object->args['options'];
 
 	// Set up the options or source PHP variables.
@@ -142,19 +145,8 @@ function autocomplete_cmb2_render_autocomplete($field_object, $escaped_value, $o
 	// Set up the autocomplete field.  Replace the '_' with '-' to not interfere with the ID from CMB2. 
 	$id = str_replace('_', '-', $field_object->args['id']);
 
-	if (isset($field_object->args['repeatable_class'])) { 
-
-		// Find out which one of the group it is.
-		for ($i = 0; $i < count($field_object->value); ++$i) {
-			if ($field_object->escaped_value === $field_object->value[$i]) {
-				break;
-			}
-		}
-
-		$id = $field_object->args['id']."-{$i}";
-	}
-
-	echo '<input size="50" id="'.$id.'" value="'.$value.'"'.
+	// Don't use the ID on repeatable elements as it won't change; use the class instead.
+	echo '<input size="50"'.(empty($repeatable_class) ? ' id="'.$id.'"' : '') . ' value="'.htmlspecialchars($value).'"'.
 		(!empty($repeatable_class) ? ' class="'.$repeatable_class.'"' : '').'/>';
 
 	if (!$field_object->args['repeatable'] && isset($field_object->args['desc'])) {
@@ -164,7 +156,7 @@ function autocomplete_cmb2_render_autocomplete($field_object, $escaped_value, $o
 	// Now, set up the script.
 	?>
 	<script>
-		(function($) {
+		jQuery(document).ready(function($) {
 			var options = [];
 			var nameToValue = [];
 			
@@ -226,7 +218,7 @@ function autocomplete_cmb2_render_autocomplete($field_object, $escaped_value, $o
 			<?php
 			}
 			?>
-		})(jQuery);
+		});
 	</script>
 	<?php
 }
@@ -239,7 +231,7 @@ function autocomplete_cmb2_render_autocomplete($field_object, $escaped_value, $o
  * @param string $like_test used to query for posts like a certain value
  * @return array
  */
-function autocomplete_cmb2_get_post_options_using_post_type($post_type, $include_empty = false, $like_test = '') {
+function autocomplete_cmb2_get_post_options_using_post_type($post_type, $include_empty = false, $like_test = '%%') {
 
 	// Use a query instead of "get_posts" to save on a ton of memory.
 	global $wpdb;
@@ -250,18 +242,12 @@ function autocomplete_cmb2_get_post_options_using_post_type($post_type, $include
 		$post_type_query = "('".$post_type."')";
 	}
 
-	if ($like_test !== '') {
-		$like_test = "AND post_title LIKE '%{$like_test}%'";
-	}
-
-	$querystr = "
-    SELECT ID, post_title
+	$posts = $wpdb->get_results($wpdb->prepare("
+		SELECT ID, post_title
     FROM $wpdb->posts 
-    WHERE post_status = 'publish' AND post_type IN $post_type_query {$like_test}
+    WHERE post_status = 'publish' AND post_type IN $post_type_query AND post_title LIKE %s
     ORDER BY post_title ASC
- 	";
-
-	$posts = $wpdb->get_results($querystr, OBJECT);
+ 	", $like_test), OBJECT);
 
 	if ($include_empty) {
 		$post_options = array(array('name' => '--- Select ---', 'value' => ''));
@@ -281,18 +267,18 @@ function autocomplete_cmb2_get_post_options_using_post_type($post_type, $include
 
 
 /**
- * Gets the jQuery autocomplete widget ready.  We need to make sure this is in the head, so it isn't enough
- * to just enqueue it, because other plugins may put it in the footer.
+ * Gets the jQuery autocomplete widget ready.
  */
 function autocomplete_cmb2_admin_enqueue_scripts() {
-	wp_enqueue_script('jquery-ui-header', '//code.jquery.com/ui/1.11.2/jquery-ui.min.js');
+	wp_enqueue_script('wp-jquery-ui-autocomplete');
+	wp_enqueue_style('wp-jquery-ui-autocomplete');
 }
 
 /**
  * Gets the post options in JSON format for the autocomplete
  */
 function autocomplete_cmb2_get_post_autocomplete_options() {
-	die(json_encode(autocomplete_cmb2_get_post_options_using_post_type('post', false, mysql_real_escape_string($_GET['q']))));
+	die(json_encode(autocomplete_cmb2_get_post_options_using_post_type('post', false, '%'.$_GET['q'].'%')));
 }
 
 add_action('cmb2_render_autocomplete', 'autocomplete_cmb2_render_autocomplete', 10, 5);
