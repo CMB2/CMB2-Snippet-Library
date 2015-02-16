d3059fdd5093712258a1123b9e2b200b628685b0
Justin Sternberg <justin@dsgnwrks.pro>
2015-02-15 20:22:44 -0500

    Clean up white-space situation, and use new metabox/field registration api

--- custom-field-types/autocomplete-field-type.php
+++ custom-field-types/autocomplete-field-type.php
@@ -2,87 +2,90 @@
 
 /*
  * Plugin Name: CMB2 Custom Field Type - Autocomplete
- * Description: Makes available an autocomplete custom field type.  It is a little complex but is very flexible, and it is a great option when you have
- * 							way too many things to put in a select.
- *
- *							It uses a hidden field for the CMB2 data, and puts the value from the input used for the autocomplete in the hidden field on blur of the 
- *							autocomplete field so it always gets populated.
- *
- *							There are two types available: options pre-built and one that uses a remote source.  To use the first option, simply build it like a select
- *							with the standard CMB2 "options" argument.  For a remote source, you will use the "source" argument that corresponds to an AJAX function.  
- *							Then, you pass in a "matching_function" to look up the selected autocomplete value using the CMB2 field value.
- *
- *							It can also be used as a repeatable field if a "repeatable_class" argument is passed when constructing the field.  This argument is 
- *							necessary to allow for mapping the autocomplete jQuery UI calls to the appropriate field.
- *
- *							The example fields in this plugin demonstrate all types: pre-built single field, pre-built repeatable field, remote single field
- *							and remote repeatable field.
+ * Description: Makes available an autocomplete custom field type.
  * Author: johnsonpaul1014
  * Version: 1.0.0
  */
 
 /**
- * Define the metabox and field configurations.
  *
- * @param  array $meta_boxes
- * @return array
+ * It is a little complex but is very flexible, and it is a great option when you have
+ * way too many things to put in a select.
+ *
+ * It uses a hidden field for the CMB2 data, and puts the value from the input used for the
+ * autocomplete in the hidden field on blur of the autocomplete field so it always gets populated.
+ *
+ * There are two types available: options pre-built and one that uses a remote source.
+ * To use the first option, simply build it like a select with the standard CMB2 "options" argument.
+ * For a remote source, you will use the "source" argument that corresponds to an AJAX function.
+ * Then, you pass in a "matching_function" to look up the selected autocomplete value using the
+ * CMB2 field value.
+ *
+ * It can also be used as a repeatable field if a "repeatable_class" argument is passed when
+ * constructing the field. This argument is necessary to allow for mapping the autocomplete
+ * jQuery UI calls to the appropriate field.
+ *
+ * The example fields in this plugin demonstrate all types: pre-built single field,
+ * pre-built repeatable field, remote single field and remote repeatable field.
+ *
  */
-function autocomplete_cmb2_meta_boxes(array $meta_boxes) {
+
+/**
+ * Define the metabox and field configurations.
+ */
+function autocomplete_cmb2_meta_boxes() {
 
 	// Start with an underscore to hide fields from custom fields list
 	$prefix = '_autocomplete_cmb2_';
 
-	$meta_boxes['autocomplete_test'] = array(
+	$cmb = new_cmb2_box( array(
 		'id' => 'autocomplete_test',
 		'title' => __('Autocomplete Field Examples', 'autocomplete_cmb2'),
 		'object_types' => array('post'),
-		'fields'        => array(
-			array(
-				'name' => __('Related Fruit', 'autocomplete_cmb2'),
-				'desc' => __('Fruit that is related to this post', 'autocomplete_cmb2'),
-				'id' => $prefix.'related_fruit',
-				'type' => 'autocomplete',
-				'options' => array(
-					array('value' => 1, 'name' => 'Apple'),
-					array('value' => 2, 'name' => 'Orange'),
-					array('value' => 3, 'name' => 'Grape')
-				)
-			),
-			array(
-				'name' => __('Related Fruits', 'autocomplete_cmb2'),
-				'desc' => __('Repeatable related fruits', 'autocomplete_cmb2'),
-				'id' => $prefix.'related_fruits',
-				'type' => 'autocomplete',
-				'repeatable' => true,
-				'repeatable_class' => 'related-fruits',
-				'options' => array(
-					array('value' => 1, 'name' => 'Apple'),
-					array('value' => 2, 'name' => 'Orange'),
-					array('value' => 3, 'name' => 'Grape')
-				)
-			),
-			array(
-				'name' => __('Related Post', 'autocomplete_cmb2'),
-				'desc' => __('Post that is related to this one', 'autocomplete_cmb2'),
-				'id' => $prefix.'related_post',
-				'type' => 'autocomplete',
-				'source' => 'get_post_options',
-				'mapping_function' => 'autocomplete_cmb2_get_post_title_from_id'
-			),
-			array(
-				'name' => __('Related Posts', 'autocomplete_cmb2'),
-				'desc' => __('Posts that are related to this one', 'autocomplete_cmb2'),
-				'id' => $prefix.'related_posts',
-				'repeatable' => true,
-				'type' => 'autocomplete',
-				'source' => 'get_post_options',
-				'repeatable_class' => 'related-posts',
-				'mapping_function' => 'autocomplete_cmb2_get_post_title_from_id'
-			)
-		)
-	);
+	) );
 
-	return $meta_boxes;
+	$cmb->add_field( array(
+		'name' => __('Related Fruit', 'autocomplete_cmb2'),
+		'desc' => __('Fruit that is related to this post', 'autocomplete_cmb2'),
+		'id' => $prefix.'related_fruit',
+		'type' => 'autocomplete',
+		'options' => array(
+			array('value' => 1, 'name' => 'Apple'),
+			array('value' => 2, 'name' => 'Orange'),
+			array('value' => 3, 'name' => 'Grape')
+		)
+	) );
+	$cmb->add_field( array(
+		'name' => __('Related Fruits', 'autocomplete_cmb2'),
+		'desc' => __('Repeatable related fruits', 'autocomplete_cmb2'),
+		'id' => $prefix.'related_fruits',
+		'type' => 'autocomplete',
+		'repeatable' => true,
+		'repeatable_class' => 'related-fruits',
+		'options' => array(
+			array('value' => 1, 'name' => 'Apple'),
+			array('value' => 2, 'name' => 'Orange'),
+			array('value' => 3, 'name' => 'Grape')
+		)
+	) );
+	$cmb->add_field( array(
+		'name' => __('Related Post', 'autocomplete_cmb2'),
+		'desc' => __('Post that is related to this one', 'autocomplete_cmb2'),
+		'id' => $prefix.'related_post',
+		'type' => 'autocomplete',
+		'source' => 'get_post_options',
+		'mapping_function' => 'autocomplete_cmb2_get_post_title_from_id'
+	) );
+	$cmb->add_field( array(
+		'name' => __('Related Posts', 'autocomplete_cmb2'),
+		'desc' => __('Posts that are related to this one', 'autocomplete_cmb2'),
+		'id' => $prefix.'related_posts',
+		'repeatable' => true,
+		'type' => 'autocomplete',
+		'source' => 'get_post_options',
+		'repeatable_class' => 'related-posts',
+		'mapping_function' => 'autocomplete_cmb2_get_post_title_from_id'
+	) );
 }
 
 /**
@@ -105,11 +108,11 @@ function autocomplete_cmb2_get_post_title_from_id($id) {
  * Renders the autocomplete type
  *
  * @param CMB2_Field $field_object
- * @param string $escaped_value The value of this field passed through the escaping filter. It defaults to sanitize_text_field. 
- *                              If you need the unescaped value, you can access it via $field_type_object->value().
+ * @param string $escaped_value The value of this field passed through the escaping filter. It defaults to sanitize_text_field.
+ *                 If you need the unescaped value, you can access it via $field_type_object->value().
  * @param string $object_id The id of the object you are working with. Most commonly, the post id.
- * @param string $object_type The type of object you are working with. Most commonly, post (this applies to all post-types), 
- *                             but could also be comment, user or options-page.
+ * @param string $object_type The type of object you are working with. Most commonly, post (this applies to all post-types),
+ *                but could also be comment, user or options-page.
  * @param CMB2_Object $field_type_object This is an instance of the CMB2 object and gives you access to all of the methods that CMB2 uses to build its field types.
  */
 function autocomplete_cmb2_render_autocomplete($field_object, $escaped_value, $object_id, $object_type, $field_type_object) {
@@ -142,7 +145,7 @@ function autocomplete_cmb2_render_autocomplete($field_object, $escaped_value, $o
 		}
 	}
 
-	// Set up the autocomplete field.  Replace the '_' with '-' to not interfere with the ID from CMB2. 
+	// Set up the autocomplete field.  Replace the '_' with '-' to not interfere with the ID from CMB2.
 	$id = str_replace('_', '-', $field_object->args['id']);
 
 	// Don't use the ID on repeatable elements as it won't change; use the class instead.
@@ -159,15 +162,15 @@ function autocomplete_cmb2_render_autocomplete($field_object, $escaped_value, $o
 		jQuery(document).ready(function($) {
 			var options = [];
 			var nameToValue = [];
-			
+
 			<?php
 
-			if (!empty($options)) { 
+			if (!empty($options)) {
 				foreach ($options as $option) {
 					echo "options.push('".addcslashes($option['name'], "'")."');\r\n";
 					echo "nameToValue['".addcslashes($option['name'], "'")."'] = '".$option['value']."';\r\n";
 				}
-			} 
+			}
 
 			if (!empty($repeatable_class)) { ?>
 			$('.<?php echo $repeatable_class; ?>').each(function(i, el) {
@@ -183,14 +186,14 @@ function autocomplete_cmb2_render_autocomplete($field_object, $escaped_value, $o
 							 data: {
 								action: '<?php echo $source; ?>',
 								q: request.term
-							 }, 
+							 },
 							 success: function(data) {
-								
+
 								// Set up options and name to value for this returned set.
 								var values = $.parseJSON(data);
 								options = [];
 								nameToValue = [];
-								
+
 								for (optionI in values) {
 									var option = values[optionI];
 									options.push(option.name);
@@ -201,7 +204,7 @@ function autocomplete_cmb2_render_autocomplete($field_object, $escaped_value, $o
 							}
 						 });
 						} <?php } else {
-							echo 'options'; 
+							echo 'options';
 						} ?>
 			});
 
@@ -209,8 +212,8 @@ function autocomplete_cmb2_render_autocomplete($field_object, $escaped_value, $o
 			$(<?php echo empty($repeatable_class) ? "'#".$id."'" : 'this'; ?>).blur(function(e) {
 				$(this).prev('input').val(nameToValue[$(this).val()]);
 			});
-			
-			<?php 
+
+			<?php
 
 			if (!empty($repeatable_class)) { ?>
 					}
@@ -225,7 +228,7 @@ function autocomplete_cmb2_render_autocomplete($field_object, $escaped_value, $o
 
 /**
  * Gets post options using a post type
- * 
+ *
  * @param mixed $post_type array or string of post type(s)
  * @param boolean $include_empty whether or not to include an empty value
  * @param string $like_test used to query for posts like a certain value
@@ -244,7 +247,7 @@ function autocomplete_cmb2_get_post_options_using_post_type($post_type, $include
 
 	$posts = $wpdb->get_results($wpdb->prepare("
 		SELECT ID, post_title
-    FROM $wpdb->posts 
+    FROM $wpdb->posts
     WHERE post_status = 'publish' AND post_type IN $post_type_query AND post_title LIKE %s
     ORDER BY post_title ASC
  	", $like_test), OBJECT);
@@ -284,4 +287,4 @@ function autocomplete_cmb2_get_post_autocomplete_options() {
 add_action('cmb2_render_autocomplete', 'autocomplete_cmb2_render_autocomplete', 10, 5);
 add_action('admin_enqueue_scripts', 'autocomplete_cmb2_admin_enqueue_scripts');
 add_action('wp_ajax_get_post_options', 'autocomplete_cmb2_get_post_autocomplete_options');
-add_filter('cmb2_meta_boxes', 'autocomplete_cmb2_meta_boxes');
+add_filter('cmb2_init', 'autocomplete_cmb2_meta_boxes');
