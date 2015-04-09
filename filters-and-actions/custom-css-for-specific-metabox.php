74b8ab4a3a8311ec9ae577d4df030500249cd6e5
Justin Sternberg <justin@dsgnwrks.pro>
2015-04-09 13:21:35 -0400

    Add custom css example

---
+++ filters-and-actions/custom-css-for-specific-metabox.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Add custom CSS which only loads for a particular metabox
+ */
+
+/**
+ * Setup the metabox
+ */
+function js_custom_css_for_metabox() {
+	$cmb = new_cmb2_box( array(
+		'id'           => 'custom_css_test',
+		'title'        => __( 'Custom CSS Test', 'cmb2' ),
+		'object_types' => array( 'page', ),
+	) );
+
+	$cmb->add_field( array(
+		'id'          => '_cmb2_test_text',
+		'type'        => 'text',
+	) );
+}
+add_action( 'cmb2_init', 'js_custom_css_for_metabox' );
+
+function js_add_custom_css_for_metabox( $post_id, $cmb ) {
+	?>
+	<style type="text/css" media="screen">
+		#custom_css_test .regular-text {
+		  width: 99%;
+		}
+	</style>
+	<?php
+}
+add_action( 'cmb2_after_post_form_custom_css_test', 'js_add_custom_css_for_metabox', 10, 2 );
