cd2dd5baa78437a3cf5cae42c9b140109b8d0d88
Justin Sternberg <justin@dsgnwrks.pro>
2015-04-09 13:21:27 -0400

    fix spacing

--- filters-and-actions/cmb2_init_$cmb_id-add-fields.php
+++ filters-and-actions/cmb2_init_$cmb_id-add-fields.php
@@ -7,10 +7,10 @@ function yourprefix_add_new_field_to_top_of_demo_metabox( $cmb ) {
 
 	$cmb->add_field(
 		array(
-			'name'       => __( 'New at the top', 'your_textdomain' ),
-			'desc'       => __( 'Using the "cmb2_init_{$cmb_id}" hook', 'your_textdomain' ),
-			'id'         => '_new_at_the_top',
-			'type'       => 'text',
+			'name' => __( 'New at the top', 'your_textdomain' ),
+			'desc' => __( 'Using the "cmb2_init_{$cmb_id}" hook', 'your_textdomain' ),
+			'id'   => '_new_at_the_top',
+			'type' => 'text',
 		),
 		'',
 		1
