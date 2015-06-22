f5eb7d03ee3c3d9a03eb9b312a26228c93f574fe
Justin Sternberg <justin@dsgnwrks.pro>
2015-06-22 09:25:38 -0400

    add network admin menu example

---
+++ options-and-settings-pages/network-options-cmb.php
@@ -0,0 +1,181 @@
+<?php
+/**
+ * CMB2 Network Settings
+ * @version 0.1.0
+ */
+class Myprefix_Network_Admin {
+
+	/**
+ 	 * Option key, and option page slug
+ 	 * @var string
+ 	 */
+	private $key = 'myprefix_options';
+
+	/**
+ 	 * Settings page metabox id
+ 	 * @var string
+ 	 */
+	private $metabox_id = 'myprefix_option_metabox';
+
+	/**
+	 * Settings Page title
+	 * @var string
+	 */
+	protected $title = '';
+
+	/**
+	 * Settings Page hook
+	 * @var string
+	 */
+	protected $options_page = '';
+
+	/**
+	 * Constructor
+	 * @since 0.1.0
+	 */
+	public function __construct() {
+		// Set our title
+		$this->title = __( 'Network Settings', 'myprefix' );
+	}
+
+	/**
+	 * Initiate our hooks
+	 * @since 0.1.0
+	 */
+	public function hooks() {
+		add_action( 'admin_init', array( $this, 'init' ) );
+		add_action( 'network_admin_menu', array( $this, 'add_options_page' ) );
+		add_action( 'cmb2_init', array( $this, 'add_options_page_metabox' ) );
+		// Override CMB's getter
+		add_filter( 'cmb2_override_option_get_'. $this->key, array( $this, 'get_override' ), 10, 2 );
+		// Override CMB's setter
+		add_filter( 'cmb2_override_option_save_'. $this->key, array( $this, 'update_override' ), 10, 2 );
+
+	}
+
+	/**
+	 * Register our setting to WP
+	 * @since  0.1.0
+	 */
+	public function init() {
+		register_setting( $this->key, $this->key );
+	}
+
+	/**
+	 * Add menu options page
+	 * @since 0.1.0
+	 */
+	public function add_options_page() {
+		$this->options_page = add_menu_page( $this->title, $this->title, 'manage_options', $this->key, array( $this, 'admin_page_display' ) );
+		// add_action( "admin_head-{$this->options_page}", array( $this, 'enqueue_js' ) );
+	}
+
+	/**
+	 * Admin page markup. Mostly handled by CMB2
+	 * @since  0.1.0
+	 */
+	public function admin_page_display() {
+		?>
+		<div class="wrap cmb2-options-page <?php echo $this->key; ?>">
+			<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+			<?php cmb2_metabox_form( $this->metabox_id, $this->key ); ?>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Add the options metabox to the array of metaboxes
+	 * @since  0.1.0
+	 */
+	function add_options_page_metabox() {
+
+		$cmb = new_cmb2_box( array(
+			'id'      => $this->metabox_id,
+			'hookup'  => false,
+			'show_on' => array(
+				// These are important, don't remove
+				'key'   => 'options-page',
+				'value' => array( $this->key, )
+			),
+		) );
+
+		// Set our CMB2 fields
+
+		$cmb->add_field( array(
+			'name' => __( 'Test Text', 'myprefix' ),
+			'desc' => __( 'field description (optional)', 'myprefix' ),
+			'id'   => 'test_text',
+			'type' => 'text',
+			'default' => 'Default Text',
+		) );
+
+		$cmb->add_field( array(
+			'name'    => __( 'Test Color Picker', 'myprefix' ),
+			'desc'    => __( 'field description (optional)', 'myprefix' ),
+			'id'      => 'test_colorpicker',
+			'type'    => 'colorpicker',
+			'default' => '#bada55',
+		) );
+
+	}
+
+	/**
+	 * Replaces get_option with get_site_option
+	 * @since  0.1.0
+	 */
+	public function get_override( $test, $default = false ) {
+		return get_site_option( $this->key, $default );
+	}
+
+	/**
+	 * Replaces update_option with update_site_option
+	 * @since  0.1.0
+	 */
+	public function update_override( $test, $option_value ) {
+		return update_site_option( $this->key, $option_value );
+	}
+
+	/**
+	 * Public getter method for retrieving protected/private variables
+	 * @since  0.1.0
+	 * @param  string  $field Field to retrieve
+	 * @return mixed          Field value or exception is thrown
+	 */
+	public function __get( $field ) {
+		// Allowed fields to retrieve
+		if ( in_array( $field, array( 'key', 'metabox_id', 'title', 'options_page' ), true ) ) {
+			return $this->{$field};
+		}
+
+		throw new Exception( 'Invalid property: ' . $field );
+	}
+
+}
+
+/**
+ * Helper function to get/return the Myprefix_Network_Admin object
+ * @since  0.1.0
+ * @return Myprefix_Network_Admin object
+ */
+function myprefix_network_admin() {
+	static $object = null;
+	if ( is_null( $object ) ) {
+		$object = new Myprefix_Network_Admin();
+		$object->hooks();
+	}
+
+	return $object;
+}
+
+/**
+ * Wrapper function around cmb2_get_option
+ * @since  0.1.0
+ * @param  string  $key Options array key
+ * @return mixed        Option value
+ */
+function myprefix_get_network_option( $key = '' ) {
+	return cmb2_get_option( myprefix_admin()->key, $key );
+}
+
+// Get it started
+myprefix_admin();
