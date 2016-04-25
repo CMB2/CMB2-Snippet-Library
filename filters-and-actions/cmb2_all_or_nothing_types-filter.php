e751e425d91b142515c88e5cfddd9f2e9a2057de
Justin Sternberg <justin@dsgnwrks.pro>
2016-04-25 12:05:21 -0400

    Document cmb2_all_or_nothing_types filter

---
+++ filters-and-actions/cmb2_all_or_nothing_types-filter.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * CMB2 has a defined set of "all or nothing types".
+ * This filter allows us to change that.
+ */
+
+function add_select2_to_all_or_nothing_types( $types ) {
+	$types[] = 'select2',
+	return $types;
+}
+add_filter( 'cmb2_all_or_nothing_types', 'add_select2_to_all_or_nothing_types' );
