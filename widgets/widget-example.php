e83b2adb0701b95ae33c24c19d8c353e3460ae65
Justin Sternberg <justin@dsgnwrks.pro>
2016-07-05 09:22:17 -0400

    Early alpha version of CMB2 used for widgets

--- widgets/widget-example.php
+++ widgets/widget-example.php
@@ -1,24 +1,249 @@
 <?php
 /**
- * This is a sample widget to see if we can use CMB2 to power widgets
- *
- * @version 0.1.0
+ * Plugin Name: CMB2 Widget Boilerplate
+ * Description: A boilerplate for building widgets with CMB2. Early alpha version. NEEDS WORK.
  */
-class CMB2_Widget_Example extends WP_Widget {
+
+// Exit if accessed directly
+if ( ! defined ( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * @todo Properly hook in JS events, etc. Fields which require JS are not working.
+ * @todo Fix css styling. Probably needs a sep. CSS file enqueued for widgets.
+ */
+class CMB2_Widget_Boilerplate extends WP_Widget {
 
 	/**
-	 * Setup the widget
-	 * @since  0.1.0
-	 * @return void
+	 * Unique identifier for this widget.
+	 *
+	 * Will also serve as the widget class.
+	 *
+	 * @var string
 	 */
-	public function __construct()
-	{
-		// Widget options
-		$options = array(
-			'customize_selective_refresh' => true,
+	protected $widget_slug = 'cmb2-widget-boilerplate-slug';
+
+	/**
+	 * Shortcode name for this widget
+	 *
+	 * @var string
+	 */
+	protected static $shortcode = 'cmb2_widget';
+
+	/**
+	 * This widget's CMB2 instance.
+	 *
+	 * @var CMB2
+	 */
+	protected $cmb2 = null;
+
+	/**
+	 * Array of default values for widget settings.
+	 *
+	 * @var array
+	 */
+	protected $defaults = array();
+
+	/**
+	 * Store the instance properties as property
+	 *
+	 * @var array
+	 */
+	protected $_instance = array();
+
+	/**
+	 * Array of CMB2 fields args.
+	 *
+	 * @var array
+	 */
+	protected $cmb2_fields = array();
+
+	/**
+	 * Contruct widget.
+	 */
+	public function __construct() {
+
+		parent::__construct(
+			$this->widget_slug,
+			esc_html__( 'CMB2 Widget Boilerplate Title', 'your-textdomain' ),
+			array(
+				'classname' => $this->widget_slug,
+				'customize_selective_refresh' => true,
+				'description' => esc_html__( 'A CMB2 widget boilerplate description.', 'your-textdomain' ),
+			)
 		);
 
-		// Set the options in the widget
+		$this->defaults = array(
+			'title' => esc_html__( 'CMB2 Widget Title', 'your-textdomain' ),
+			'image' => '',
+			'desc'  => '',
+			'color' => '#bada55',
+		);
+
+		$this->cmb2_fields = array(
+			array(
+				'name'   => 'Title',
+				'id_key' => 'title',
+				'id'     => 'title',
+				'type'   => 'text',
+			),
+			array(
+				'name'    => 'Image',
+				'desc'    => 'Upload an image or enter an URL.',
+				'id_key'  => 'image',
+				'id'      => 'image',
+				'type'    => 'file',
+				'options' => array(
+					'url'	=> false
+				),
+				'text' => array(
+					'add_upload_file_text' => 'Upload An Image'
+				),
+			),
+			array(
+				'name'   => 'Description',
+				'id_key' => 'desc',
+				'id'     => 'desc',
+				'type'   => 'textarea',
+			),
+			array(
+				'name'   => 'Color',
+				'id_key' => 'color',
+				'id'     => 'color',
+				'type'   => 'colorpicker',
+			),
+		);
+
+		add_action( 'save_post',    array( $this, 'flush_widget_cache' ) );
+		add_action( 'deleted_post', array( $this, 'flush_widget_cache' ) );
+		add_action( 'switch_theme', array( $this, 'flush_widget_cache' ) );
+		add_shortcode( self::$shortcode, array( __CLASS__, 'get_widget' ) );
+	}
+
+	/**
+	 * Delete this widget's cache.
+	 *
+	 * Note: Could also delete any transients
+	 * delete_transient( 'some-transient-generated-by-this-widget' );
+	 */
+	public function flush_widget_cache() {
+		wp_cache_delete( $this->id, 'widget' );
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @param  array  $args      The widget arguments set up when a sidebar is registered.
+	 * @param  array  $instance  The widget settings as set by user.
+	 */
+	public function widget( $args, $instance ) {
+
+		echo self::get_widget( array(
+			'args'     => $args,
+			'instance' => $instance,
+			'cache_id' => $this->id, // whatever the widget id is
+		) );
+
+	}
+
+	/**
+	 * Return the widget/shortcode output
+	 *
+	 * @param  array  $atts Array of widget/shortcode attributes/args
+	 * @return string       Widget output
+	 */
+	public static function get_widget( $atts ) {
+		$widget = '';
+
+		// Set up default values for attributes
+		$atts = shortcode_atts(
+			array(
+				// Ensure variables
+				'before_widget' => '',
+				'after_widget'  => '',
+				'before_title'  => '',
+				'after_title'   => '',
+				'cache_id'      => '',
+				'flush_cache'   => isset( $_GET['delete-trans'] ), // Check for cache-buster
+			),
+			isset( $atts['args'] ) ? (array) $atts['args'] : array(),
+			self::$shortcode
+		);
+
+		$instance = shortcode_atts(
+			$this->defaults,
+			isset( $atts['instance'] ) ? (array) $atts['instance'] : array(),
+			self::$shortcode
+		);
+
+		/*
+		 * If cache_id is not passed, we're not using the widget (but the shortcode),
+		 * so generate a hash cache id from the shortcode arguments
+		 */
+		if ( empty( $atts['cache_id'] ) ) {
+			$atts['cache_id'] = md5( serialize( $atts ) );
+		}
+
+		// Get from cache unless being requested not to
+		$widget = ! $atts['flush_cache']
+			? wp_cache_get( $atts['cache_id'], 'widget' )
+			: '';
+
+		// If $widget is empty, rebuild our cache
+		if ( empty( $widget ) ) {
+
+			$widget = '';
+
+			// Before widget hook
+			$widget .= $atts['before_widget'];
+
+			$widget .= '<div style="background-color:'. esc_attr( $instance['color'] ) .'">';
+
+			// Title
+			$widget .= ( $atts['title'] ) ? $atts['before_title'] . esc_html( $instance['title'] ) . $atts['after_title'] : '';
+
+			$widget .= wpautop( wp_kses_post( $instance['desc'] ) );
+
+			$widget .= '</div>';
+
+			// After widget hook
+			$widget .= $atts['after_widget'];
+
+			wp_cache_set( $atts['cache_id'], $widget, 'widget', WEEK_IN_SECONDS );
+
+		}
+
+		return $widget;
+	}
+
+	/**
+	 * Update form values as they are saved.
+	 *
+	 * @param  array  $new_instance  New settings for this instance as input by the user.
+	 * @param  array  $old_instance  Old settings for this instance.
+	 * @return array  Settings to save or bool false to cancel saving.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$this->flush_widget_cache();
+		$sanitized = $this->cmb2( true )->get_sanitized_values( $new_instance );
+		return $sanitized;
+	}
+	/**
+	 * Back-end widget form with defaults.
+	 *
+	 * @param  array  $instance  Current settings.
+	 */
+	public function form( $instance ) {
+		// If there are no settings, set up defaults
+		$this->_instance = wp_parse_args( (array) $instance, $this->defaults );
+
+		$cmb2 = $this->cmb2();
+
+		$cmb2->object_id( $this->option_name );
+		CMB2_hookup::enqueue_cmb_css();
+		CMB2_hookup::enqueue_cmb_js();
+		$cmb2->show_form();
 	}
 
 	/**
@@ -26,125 +251,52 @@ class CMB2_Widget_Example extends WP_Widget {
 	 * @since  0.1.0
 	 * @return CMB2
 	 */
-	public function fields()
-	{
+	public function cmb2( $saving = false ) {
+
 		// Create a new box in the class
-		$fields = new_cmb2_box(array(
-			'id'				=> $this->option_name, // Option name is taken from the WP_Widget class.
-			'title'				=> __( 'Widget Options', 'theme' ),
-			'context'			=> 'normal',
-			'priority'			=> 'high',
-			'show_names'		=> true,
-			'show_on'			=> array(
-				'key' => 'options-page', // Tells CMB2 to handle this as an option
+		$cmb2 = new CMB2( array(
+			'id'      => $this->option_name .'_box', // Option name is taken from the WP_Widget class.
+			'hookup'  => false,
+			'show_on' => array(
+				'key'   => 'options-page', // Tells CMB2 to handle this as an option
 				'value' => array( $this->option_name )
-			)
-		));
-
-		$fields->add_field(array(
-			'name'		=> 'Image',
-			'desc'		=> 'Upload an image or enter an URL.',
-			'id'		=> 'image',
-			'type'		=> 'file',
-			'options'	=> array(
-				'url'	=> false
 			),
-			'text'		=> array(
-				'add_upload_file_text' => 'Upload An Image'
-			),
-		));
+		), $this->option_name );
 
-		$fields->add_field(array(
-			'name'		=> 'Description',
-			'id'		=> 'desc',
-			'type'		=> 'textarea'
-		));
+		foreach ( $this->cmb2_fields as $field ) {
 
-		return $fields;
+			if ( ! $saving ) {
+				$field['id'] = $this->get_field_name( $field['id'] );
+			}
+
+			$field['default_cb'] = array( $this, 'default_cb' );
+
+			$cmb2->add_field( $field );
+		}
+
+		return $cmb2;
 	}
 
 	/**
-	 * Renders the form
+	 * Sets the field default, or the field value.
 	 *
-	 * @param  object $instance
-	 * @return void
+	 * @param  array      $field_args CMB2 field args array
+	 * @param  CMB2_Field $field CMB2 Field object.
+	 *
+	 * @return mixed      Field value.
 	 */
-	public function form( $instance )
-	{
-
-		/**
-		 * Handle field registration separate from CMB2 hooks.
-		 * Unfortunately I couldn't figure out how to hook into cmb2_admin_init
-		 * before the 'form' method ran, so we're just creating an instance of CMB2
-		 */
-		$cmb2 = $this->fields();
-
-		// Remove the form and submit button, otherwise this won't work
-		$args = array(
-			'form_format' => '<input type="hidden" name="object_id" value="%2$s">',
-		);
-
-		// Echo the form
-		cmb2_print_metabox_form( $cmb2, $this->number, $args );
+	public function default_cb( $field_args, $field ) {
+		return isset( $this->_instance[ $field->args( 'id_key' ) ] )
+			? $this->_instance[ $field->args( 'id_key' ) ]
+			: null;
 	}
 
-	/**
-	 * Traditional way to save the widget settings
-	 * @since  0.1.0
-	 * @param  array $new_instance
-	 * @param  array $old_instance
-	 * @return object WP_Widget
-	 */
-	public function old_update( $new_instance, $old_instance )
-	{
-		$instance = array();
-
-		if( !empty( $new_instance ) ){
-			foreach( $new_instance as $field => $value ) {
-				$instance[$field] = $value;
-			}
-		}
-
-		return $instance;
-	}
-
-
-	/**
-	 * Save the widget settings using the $_POST global variable
-	 * and checking that our fields have been registered first.
-	 * 
-	 * @since  0.1.0
-	 * @param  array $new_instance
-	 * @param  array $old_instance
-	 * @return object WP_Widget
-	 */
-	public function update( $new_instance, $old_instance )
-	{
-		$cmb2 = $this->fields();
-		$check = array();
-
-		foreach( $_POST as $field => $value ) {
-
-			// Check if the posted data is a registered field
-			if( $cmb2->get_field_ids( $field ) ) {
-				$check[$field] = $value;
-			}
-
-		}
-		return $check;
-	}
 }
 
 /**
- * Register the widget
- *
- * @since  0.1.0
- * @return void
+ * Register this widget with WordPress.
  */
-function cmb2_widget_example_init()
-{
-	register_widget( 'CMB2_Widget_Example' );
+function register_wds_widget_boilerplate() {
+	register_widget( 'CMB2_Widget_Boilerplate' );
 }
-
-// Hook into widgets_init
-add_action( 'widgets_init', 'cmb2_widget_example_init' );
+add_action( 'widgets_init', 'register_wds_widget_boilerplate' );
