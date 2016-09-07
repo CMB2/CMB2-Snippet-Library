5f6f3687c422a1f89866002fe5eda7e16361e1c5
Justin Sternberg <me@jtsternberg.com>
2016-09-07 08:27:05 -0500

    Add caution for using the `cmb2_enqueue_css` filter.

--- filters-and-actions/disable-styles-on-front-end-forms.php
+++ filters-and-actions/disable-styles-on-front-end-forms.php
@@ -1,11 +1,15 @@
 <?php
 /**
  * This file demonstrates how to disable CMB2 styles on all front end forms.
+ * 
+ * It’s important, when using the `cmb2_enqueue_*` filters to only disable for your
+ * specific conditions/scenarios. It’s entirely possible that another plugin/theme is
+ * using CMB2 and *needs* the styles on the front-end, so blindly disabling can cause issues.
  */
 
 function yourprefix_disable_cmb2_front_end_styles( $enabled ) {
 
-	if ( ! is_admin() ) {
+	if ( ! is_admin() /* && meets_your_specific_conditions() */ ) {
 		$enabled = false;
 	}
 
