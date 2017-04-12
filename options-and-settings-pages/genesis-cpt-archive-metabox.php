3ebcab5716b7a68e0c61e054b7acf4578988a03f
Justin Sternberg <justin@dsgnwrks.pro>
2017-04-12 15:20:32 -0400

    Make CMB2 genesis archive settings officially work

--- options-and-settings-pages/genesis-cpt-archive-metabox.php
+++ options-and-settings-pages/genesis-cpt-archive-metabox.php
@@ -15,7 +15,7 @@ class Myprefix_Genesis_CPT_Settings_Metabox {
  	 * CPT slug
  	 * @var string
  	 */
-	public $post_type = 'post';
+	protected $post_type = '';
 
 	/**
  	 * CPT slug
@@ -27,7 +27,7 @@ class Myprefix_Genesis_CPT_Settings_Metabox {
  	 * Option key, and option page slug
  	 * @var string
  	 */
-	protected $key = 'genesis-cpt-archive-%1$s';
+	protected $key = 'genesis-cpt-archive-settings-%1$s';
 
 	/**
 	 * Holds an instance of CMB2
@@ -37,31 +37,39 @@ class Myprefix_Genesis_CPT_Settings_Metabox {
 	protected $cmb = null;
 
 	/**
-	 * Holds an instance of the object
+	 * Holds all instances of this class.
 	 *
 	 * @var Myprefix_Genesis_CPT_Settings_Metabox
 	 */
-	protected static $instance = null;
+	protected static $instances = array();
 
 	/**
-	 * Returns the running object
+	 * Returns an instance.
+	 *
+	 * @since  0.1.0
+	 *
+	 * @param  string $post_type Post type slug
 	 *
 	 * @return Myprefix_Genesis_CPT_Settings_Metabox
 	 */
-	public static function get_instance() {
-		if ( null === self::$instance ) {
-			self::$instance = new self();
-			self::$instance->hooks();
+	public static function get_instance( $post_type ) {
+		if ( empty( self::$instances[ $post_type ] ) ) {
+			self::$instances[ $post_type ] = new self( $post_type );
+			self::$instances[ $post_type ]->hooks();
 		}
 
-		return self::$instance;
+		return self::$instances[ $post_type ];
 	}
 
 	/**
 	 * Constructor
 	 * @since 0.1.0
 	 */
-	protected function __construct() {
+	protected function __construct( $post_type ) {
+		$this->post_type  = $post_type;
+		$this->admin_hook = sprintf( $this->admin_hook, $post_type );
+		$this->key        = sprintf( $this->key, $post_type );
+		$this->metabox_id = sprintf( $this->metabox_id, $post_type );
 	}
 
 	/**
@@ -71,12 +79,12 @@ class Myprefix_Genesis_CPT_Settings_Metabox {
 	public function hooks() {
 		add_action( 'init', array( $this, 'init' ) );
 		add_action( 'admin_menu', array( $this, 'admin_hooks' ) );
-		add_action( 'cmb2_admin_init', array( $this, 'register_metabox' ) );
+		add_action( 'cmb2_admin_init', array( $this, 'init_metabox' ) );
 	}
 
 
 	/**
-	 * Add hooks.
+	 * Initiate admin hooks.
 	 * @since  0.1.0
 	 */
 	public function init() {
@@ -89,48 +97,79 @@ class Myprefix_Genesis_CPT_Settings_Metabox {
 	 * @since 0.1.0
 	 */
 	public function admin_hooks() {
-
 		// Include CMB CSS in the head to avoid FOUC
-		add_action( "admin_print_styles-{$this->admin_hook()}", array( 'CMB2_hookup', 'enqueue_cmb_css' ) );
+		add_action( "admin_print_styles-{$this->admin_hook}", array( 'CMB2_hookup', 'enqueue_cmb_css' ) );
+
+		// Hook into the genesis cpt setttings save and add in the CMB2 sanitized values.
+		add_filter( "sanitize_option_genesis-cpt-archive-settings-{$this->post_type}", array( $this, 'add_sanitized_values' ), 999, 2 );
+
+		// Hook up our Genesis metabox.
 		add_action( 'genesis_cpt_archives_settings_metaboxes', array( $this, 'add_meta_box' ) );
 	}
 
-	public function add_meta_box( $page_hook ) {
-		$cmb = $this->register_metabox();
-
-		add_meta_box( $cmb->cmb_id, $cmb->prop( 'title' ), array( $this, 'output_metabox' ), $page_hook, 'main', $cmb->prop( 'priority' ) );
-	}
-
-	public function output_metabox() {
-		$cmb = $this->register_metabox();
-		$cmb->show_form( $cmb->object_id(), $cmb->object_type() );
+	/**
+	 * Hook up our Genesis metabox.
+	 * @since 0.1.0
+	 */
+	public function add_meta_box() {
+		$cmb = $this->init_metabox();
+		add_meta_box(
+			$cmb->cmb_id,
+			$cmb->prop( 'title' ),
+			array( $this, 'output_metabox' ),
+			$this->admin_hook,
+			$cmb->prop( 'context' ),
+			$cmb->prop( 'priority' )
+		);
 	}
 
 	/**
-	 * Register the cpt arvhive metabox.
+	 * Output our Genesis metabox.
 	 * @since  0.1.0
 	 */
-	function register_metabox() {
+	public function output_metabox() {
+		$cmb = $this->init_metabox();
+		$cmb->show_form( $cmb->object_id(), $cmb->object_type() );
+	}
+
+	public function add_sanitized_values( $new_value, $option ) {
+		if ( ! empty( $_POST ) ) {
+			$cmb = $this->init_metabox();
+
+			$new_value = array_merge(
+				$new_value,
+				$cmb->get_sanitized_values( $_POST )
+			);
+		}
+
+		return $new_value;
+	}
+
+	/**
+	 * Register our Genesis metabox and return the CMB2 instance.
+	 *
+	 * @since  0.1.0
+	 *
+	 * @return CMB2 instance.
+	 */
+	public function init_metabox() {
 		if ( null !== $this->cmb ) {
 			return $this->cmb;
 		}
 
-		$metabox_id = $this->metabox_id();
-
-		// hook in our save notices
-		add_action( "cmb2_save_options-page_fields_{$metabox_id}", array( $this, 'settings_notices' ), 10, 2 );
-
-		$this->cmb = new_cmb2_box( array(
-			'id'         => $metabox_id,
-			'title'      => __( 'I\'m a Genesis Archive Settings CMB2 metabox', 'myprefix' ),
-			'hookup'     => false,
-			'cmb_styles' => false,
-			'show_on'    => array(
+		$this->cmb = cmb2_get_metabox( array(
+			'id'           => $this->metabox_id,
+			'title'        => __( 'I\'m a Genesis Archive Settings CMB2 metabox', 'myprefix' ),
+			'hookup'       => false, // We'll handle ourselves. (add_sanitized_values())
+			'cmb_styles'   => false, // We'll handle ourselves. (admin_hooks())
+			'context'      => 'main', // Important for Genesis.
+			'object_types' => array( $this->admin_hook ),
+			'show_on'      => array(
 				// These are important, don't remove
 				'key'   => 'options-page',
-				'value' => array( $this->key(), )
+				'value' => array( $this->key, )
 			),
-		) );
+		), $this->key, 'options-page' );
 
 		// Set our CMB2 fields
 
@@ -150,36 +189,7 @@ class Myprefix_Genesis_CPT_Settings_Metabox {
 			'default' => '#bada55',
 		) );
 
-	}
-
-	/**
-	 * Register settings notices for display
-	 *
-	 * @since  0.1.0
-	 * @param  int   $object_id Option key
-	 * @param  array $updated   Array of updated fields
-	 * @return void
-	 */
-	public function settings_notices( $object_id, $updated ) {
-		$key = $this->key();
-		if ( $object_id !== $key || empty( $updated ) ) {
-			return;
-		}
-
-		add_settings_error( $key . '-notices', '', __( 'Settings updated.', 'myprefix' ), 'updated' );
-		settings_errors( $key . '-notices' );
-	}
-
-	public function admin_hook() {
-		return sprintf( $this->admin_hook, $this->post_type );
-	}
-
-	public function key() {
-		return sprintf( $this->key, $this->post_type );
-	}
-
-	public function metabox_id() {
-		return sprintf( $this->metabox_id, $this->post_type );
+		return $this->cmb;
 	}
 
 	/**
@@ -190,11 +200,11 @@ class Myprefix_Genesis_CPT_Settings_Metabox {
 	 */
 	public function __get( $field ) {
 		// Allowed fields to retrieve
-		if ( in_array( $field, array( 'metabox_id', 'key', 'admin_hook' ), true ) ) {
-			return $this->{$field}();
+		if ( 'cmb' === $field ) {
+			return $this->init_metabox();
 		}
 
-		if ( in_array( $field, array( 'title', 'options_page' ), true ) ) {
+		if ( in_array( $field, array( 'metabox_id', 'post_type', 'admin_hook', 'key' ), true ) ) {
 			return $this->{$field};
 		}
 
@@ -204,14 +214,17 @@ class Myprefix_Genesis_CPT_Settings_Metabox {
 }
 
 /**
- * Helper function to get/return the Myprefix_Genesis_CPT_Settings_Metabox object
+ * Helper function to get/return the Myprefix_Genesis_CPT_Settings_Metabox object.
+ *
  * @since  0.1.0
+ *
+ * @param  string $post_type Post type slug
+ *
  * @return Myprefix_Genesis_CPT_Settings_Metabox object
  */
-function myprefix_genesis_cpt_settings() {
-	return Myprefix_Genesis_CPT_Settings_Metabox::get_instance();
+function myprefix_genesis_cpt_settings( $post_type ) {
+	return Myprefix_Genesis_CPT_Settings_Metabox::get_instance( $post_type );
 }
 
-
 // Get it started
-myprefix_genesis_cpt_settings();
+// myprefix_genesis_cpt_settings( 'custom-post-type-slug' );
