4a0b2757894a3f9a6b1e91651a11594a6e6ad2d0
Justin Sternberg <justin@dsgnwrks.pro>
2017-11-20 10:29:26 -0500

    Clean up snippet

--- options-and-settings-pages/custom-display-callback.php
+++ options-and-settings-pages/custom-display-callback.php
@@ -5,40 +5,19 @@ add_action( 'cmb2_admin_init', 'yourprefix_register_theme_options_metabox' );
  */
 function yourprefix_register_theme_options_metabox() {
 
-	/**
-	 * Registers options page menu item and form.
-	 */
 	$cmb_options = new_cmb2_box( array(
 		'id'           => 'yourprefix_theme_options_page',
-		'title'        => esc_html__( 'Theme Options', 'cmb2' ),
+		'title'        => 'Theme Options',
 		'object_types' => array( 'options-page' ),
-
-		/*
-		 * The following parameters are specific to the options-page box
-		 * Several of these parameters are passed along to add_menu_page()/add_submenu_page().
-		 */
-
-		'option_key'      => 'yourprefix_theme_options', // The option key and admin menu page slug.
-		'icon_url'        => 'dashicons-palmtree', // Menu icon. Only applicable if 'parent_slug' is left empty.
-		// 'menu_title'      => esc_html__( 'Options', 'cmb2' ), // Falls back to 'title' (above).
-		// 'parent_slug'     => 'themes.php', // Make options page a submenu item of the themes menu.
-		// 'capability'      => 'manage_options', // Cap required to view options-page.
-		// 'position'        => 1, // Menu position. Only applicable if 'parent_slug' is left empty.
-		// 'admin_menu_hook' => 'network_admin_menu', // 'network_admin_menu' to add network-level options page.
-		'display_cb'      => 'yourprefix_theme_options_page_output', // Override the options-page form output (CMB2_Hookup::options_page_output()).
-		// 'save_button'     => esc_html__( 'Save Theme Options', 'cmb2' ), // The text for the options-page save button. Defaults to 'Save'.
-		// 'disable_settings_errors' => true, // On settings pages (not options-general.php sub-pages), allows disabling.
-		// 'message_cb'      => 'yourprefix_options_page_message_callback',
+		'option_key'   => 'yourprefix_theme_options',
+		'icon_url'     => 'dashicons-palmtree',
+		'display_cb'   => 'yourprefix_theme_options_page_output', // Override the options-page form output (CMB2_Hookup::options_page_output()).
+		'description'  => 'Custom description', // Will be displayed via our display_cb.
 	) );
 
-	/**
-	 * Options fields ids only need
-	 * to be unique within this box.
-	 * Prefix is not needed.
-	 */
 	$cmb_options->add_field( array(
-		'name'    => esc_html__( 'Site Background Color', 'cmb2' ),
-		'desc'    => esc_html__( 'field description (optional)', 'cmb2' ),
+		'name'    => 'Site Background Color',
+		'desc'    => 'field description (optional)',
 		'id'      => 'bg_color',
 		'type'    => 'colorpicker',
 		'default' => '#ffffff',
