b8d2c5dbc2608b60caf3a951fd5e992a14d076b4
Justin Sternberg <justin@dsgnwrks.pro>
2017-12-23 13:35:58 -0500

    Document submenu metaboxes

---
+++ options-and-settings-pages/submenu-options-pages.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * Hook in and register a submenu options page for the Page post-type menu.
+ */
+function yourprefix_register_options_submenu_for_page_post_type() {
+
+	/**
+	 * Registers options page menu item and form.
+	 */
+	$cmb = new_cmb2_box( array(
+		'id'           => 'yourprefix_options_submenu_page',
+		'title'        => esc_html__( 'Page Options', 'cmb2' ),
+		'object_types' => array( 'options-page' ),
+
+		/*
+		 * The following parameters are specific to the options-page box
+		 * Several of these parameters are passed along to add_menu_page()/add_submenu_page().
+		 */
+
+		'option_key'      => 'yourprefix_page_options', // The option key and admin menu page slug.
+		// 'icon_url'        => '', // Menu icon. Only applicable if 'parent_slug' is left empty.
+		// 'menu_title'      => esc_html__( 'Options', 'cmb2' ), // Falls back to 'title' (above).
+		'parent_slug'     => 'edit.php?post_type=page', // Make options page a submenu item of the themes menu.
+		// 'capability'      => 'manage_options', // Cap required to view options-page.
+		// 'position'        => 1, // Menu position. Only applicable if 'parent_slug' is left empty.
+		// 'admin_menu_hook' => 'network_admin_menu', // 'network_admin_menu' to add network-level options page.
+		// 'display_cb'      => false, // Override the options-page form output (CMB2_Hookup::options_page_output()).
+		// 'save_button'     => esc_html__( 'Save Theme Options', 'cmb2' ), // The text for the options-page save button. Defaults to 'Save'.
+		// 'disable_settings_errors' => true, // On settings pages (not options-general.php sub-pages), allows disabling.
+		// 'message_cb'      => 'yourprefix_options_page_message_callback',
+	) );
+
+	$cmb->add_field( array(
+		'name'    => esc_html__( 'Background Color for Pages', 'cmb2' ),
+		'desc'    => esc_html__( 'field description (optional)', 'cmb2' ),
+		'id'      => 'bg_color',
+		'type'    => 'colorpicker',
+		'default' => '#ffffff',
+	) );
+
+}
+add_action( 'cmb2_admin_init', 'yourprefix_register_options_submenu_for_page_post_type' );
+
+/**
+ * Hook in and register a submenu options page for the Appearance menu.
+ */
+function yourprefix_register_options_submenu_appearance_menu() {
+
+	/**
+	 * Registers options page menu item and form.
+	 */
+	$cmb_options = new_cmb2_box( array(
+		'id'           => 'yourprefix_options_submenu_appearance_menu',
+		'title'        => esc_html__( 'Appearance Options', 'cmb2' ),
+		'object_types' => array( 'options-page' ),
+
+		/*
+		 * The following parameters are specific to the options-page box
+		 * Several of these parameters are passed along to add_menu_page()/add_submenu_page().
+		 */
+
+		'option_key'      => 'yourprefix_theme_appearance_options', // The option key and admin menu page slug.
+		// 'icon_url'        => '', // Menu icon. Only applicable if 'parent_slug' is left empty.
+		// 'menu_title'      => esc_html__( 'Options', 'cmb2' ), // Falls back to 'title' (above).
+		'parent_slug'     => 'themes.php', // Make options page a submenu item of the themes menu.
+		// 'capability'      => 'manage_options', // Cap required to view options-page.
+		// 'position'        => 1, // Menu position. Only applicable if 'parent_slug' is left empty.
+		// 'admin_menu_hook' => 'network_admin_menu', // 'network_admin_menu' to add network-level options page.
+		// 'display_cb'      => false, // Override the options-page form output (CMB2_Hookup::options_page_output()).
+		// 'save_button'     => esc_html__( 'Save Theme Options', 'cmb2' ), // The text for the options-page save button. Defaults to 'Save'.
+		// 'disable_settings_errors' => true, // On settings pages (not options-general.php sub-pages), allows disabling.
+		// 'message_cb'      => 'yourprefix_options_page_message_callback',
+	) );
+
+	/**
+	 * Options fields ids only need
+	 * to be unique within this box.
+	 * Prefix is not needed.
+	 */
+	$cmb_options->add_field( array(
+		'name'    => esc_html__( 'Site Background Color', 'cmb2' ),
+		'desc'    => esc_html__( 'field description (optional)', 'cmb2' ),
+		'id'      => 'bg_color',
+		'type'    => 'colorpicker',
+		'default' => '#ffffff',
+	) );
+
+}
+add_action( 'cmb2_admin_init', 'yourprefix_register_options_submenu_appearance_menu' );
