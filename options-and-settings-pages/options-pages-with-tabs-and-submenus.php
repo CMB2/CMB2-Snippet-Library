bd85852257d67a8476dc6a5c7922950e67eb8828
Justin Sternberg <justin@dsgnwrks.pro>
2017-12-23 15:48:21 -0500

    Update snippet to account for new tab_group feature in CMB2 2.4.0

--- options-and-settings-pages/options-pages-with-tabs-and-submenus.php
+++ options-and-settings-pages/options-pages-with-tabs-and-submenus.php
@@ -7,28 +7,21 @@ function yourprefix_register_main_options_metabox() {
 	/**
 	 * Registers main options page menu item and form.
 	 */
-	$main_options = new_cmb2_box( array(
+	$args = array(
 		'id'           => 'yourprefix_main_options_page',
-		'title'        => esc_html__( 'Main Options', 'cmb2' ),
+		'title'        => 'Main Options',
 		'object_types' => array( 'options-page' ),
+		'option_key'   => 'yourprefix_main_options',
+		'tab_group'    => 'yourprefix_main_options',
+		'tab_title'    => 'Main',
+	);
 
-		/*
-		 * The following parameters are specific to the options-page box
-		 * Several of these parameters are passed along to add_menu_page()/add_submenu_page().
-		 */
+	// 'tab_group' property is supported in > 2.4.0.
+	if ( version_compare( CMB2_VERSION, '2.4.0' ) ) {
+		$args['display_cb'] = 'yourprefix_options_display_with_tabs';
+	}
 
-		'option_key'      => 'yourprefix_main_options', // The option key and admin menu page slug.
-		// 'icon_url'        => 'dashicons-palmtree', // Menu icon. Only applicable if 'parent_slug' is left empty.
-		// 'menu_title'      => esc_html__( 'Options', 'cmb2' ), // Falls back to 'title' (above).
-		// 'parent_slug'     => 'themes.php', // Make options page a submenu item of the themes menu.
-		// 'capability'      => 'manage_options', // Cap required to view options-page.
-		// 'position'        => 1, // Menu position. Only applicable if 'parent_slug' is left empty.
-		// 'admin_menu_hook' => 'network_admin_menu', // 'network_admin_menu' to add network-level options page.
-		'display_cb'      => 'yourprefix_options_display_with_tabs', // Override the options-page form output (CMB2_Hookup::options_page_output()).
-		// 'save_button'     => esc_html__( 'Save Theme Options', 'cmb2' ), // The text for the options-page save button. Defaults to 'Save'.
-		// 'disable_settings_errors' => true, // On settings pages (not options-general.php sub-pages), allows disabling.
-		// 'message_cb'      => 'yourprefix_options_page_message_callback',
-	) );
+	$main_options = new_cmb2_box( $args );
 
 	/**
 	 * Options fields ids only need
@@ -36,8 +29,8 @@ function yourprefix_register_main_options_metabox() {
 	 * Prefix is not needed.
 	 */
 	$main_options->add_field( array(
-		'name'    => esc_html__( 'Site Background Color', 'cmb2' ),
-		'desc'    => esc_html__( 'field description (optional)', 'cmb2' ),
+		'name'    => 'Site Background Color',
+		'desc'    => 'field description (optional)',
 		'id'      => 'bg_color',
 		'type'    => 'colorpicker',
 		'default' => '#ffffff',
@@ -46,42 +39,58 @@ function yourprefix_register_main_options_metabox() {
 	/**
 	 * Registers secondary options page, and set main item as parent.
 	 */
-	$secondary_options = new_cmb2_box( array(
+	$args = array(
 		'id'           => 'yourprefix_secondary_options_page',
-		'title'        => esc_html__( 'Secondary Options', 'cmb2' ),
+		'menu_title'   => 'Secondary Options', // Use menu title, & not title to hide main h2.
 		'object_types' => array( 'options-page' ),
 		'option_key'   => 'yourprefix_secondary_options',
 		'parent_slug'  => 'yourprefix_main_options',
-		'display_cb'   => 'yourprefix_options_display_with_tabs',
-	) );
+		'tab_group'    => 'yourprefix_main_options',
+		'tab_title'    => 'Secondary',
+	);
+
+	// 'tab_group' property is supported in > 2.4.0.
+	if ( version_compare( CMB2_VERSION, '2.4.0' ) ) {
+		$args['display_cb'] = 'yourprefix_options_display_with_tabs';
+	}
+
+	$secondary_options = new_cmb2_box( $args );
 
 	$secondary_options->add_field( array(
-		'name'    => esc_html__( 'Test Radio', 'cmb2' ),
-		'desc'    => esc_html__( 'field description (optional)', 'cmb2' ),
+		'name'    => 'Test Radio',
+		'desc'    => 'field description (optional)',
 		'id'      => 'radio',
 		'type'    => 'radio',
 		'options' => array(
-			'option1' => esc_html__( 'Option One', 'cmb2' ),
-			'option2' => esc_html__( 'Option Two', 'cmb2' ),
-			'option3' => esc_html__( 'Option Three', 'cmb2' ),
+			'option1' => 'Option One',
+			'option2' => 'Option Two',
+			'option3' => 'Option Three',
 		),
 	) );
 
 	/**
 	 * Registers tertiary options page, and set main item as parent.
 	 */
-	$tertiary_options = new_cmb2_box( array(
+	$args = array(
 		'id'           => 'yourprefix_tertiary_options_page',
-		'title'        => esc_html__( 'Tertiary Options', 'cmb2' ),
+		'menu_title'   => 'Tertiary Options', // Use menu title, & not title to hide main h2.
 		'object_types' => array( 'options-page' ),
 		'option_key'   => 'yourprefix_tertiary_options',
 		'parent_slug'  => 'yourprefix_main_options',
-		'display_cb'   => 'yourprefix_options_display_with_tabs',
-	) );
+		'tab_group'    => 'yourprefix_main_options',
+		'tab_title'    => 'Tertiary',
+	);
+
+	// 'tab_group' property is supported in > 2.4.0.
+	if ( version_compare( CMB2_VERSION, '2.4.0' ) ) {
+		$args['display_cb'] = 'yourprefix_options_display_with_tabs';
+	}
+
+	$tertiary_options = new_cmb2_box( $args );
 
 	$tertiary_options->add_field( array(
-		'name' => esc_html__( 'Test Text Area for Code', 'cmb2' ),
-		'desc' => esc_html__( 'field description (optional)', 'cmb2' ),
+		'name' => 'Test Text Area for Code',
+		'desc' => 'field description (optional)',
 		'id'   => 'textarea_code',
 		'type' => 'textarea_code',
 	) );
@@ -96,14 +105,15 @@ add_action( 'cmb2_admin_init', 'yourprefix_register_main_options_metabox' );
  * @param CMB2_Options_Hookup $cmb_options The CMB2_Options_Hookup object.
  */
 function yourprefix_options_display_with_tabs( $cmb_options ) {
-	$tabs = yourprefix_options_page_tabs( $cmb_options->cmb->prop( 'display_cb' ) );
+	$tabs = yourprefix_options_page_tabs( $cmb_options );
 	?>
 	<div class="wrap cmb2-options-page option-<?php echo $cmb_options->option_key; ?>">
-		<?php // You can optionally remove this get_admin_page_title() line to use the tabs only ?>
-		<h2><?php echo wp_kses_post( get_admin_page_title() ); ?></h2>
+		<?php if ( get_admin_page_title() ) : ?>
+			<h2><?php echo wp_kses_post( get_admin_page_title() ); ?></h2>
+		<?php endif; ?>
 		<h2 class="nav-tab-wrapper">
-			<?php foreach ( $tabs as $option_key => $tab ) : ?>
-				<a class="<?php echo $tab['class']; ?>" href="<?php menu_page_url( $option_key ); ?>"><?php echo wp_kses_post( $tab['title'] ); ?></a>
+			<?php foreach ( $tabs as $option_key => $tab_title ) : ?>
+				<a class="nav-tab<?php if ( isset( $_GET['page'] ) && $option_key === $_GET['page'] ) : ?> nav-tab-active<?php endif; ?>" href="<?php menu_page_url( $option_key ); ?>"><?php echo wp_kses_post( $tab_title ); ?></a>
 			<?php endforeach; ?>
 		</h2>
 		<form class="cmb-form" action="<?php echo esc_url( admin_url( 'admin-post.php' ) ); ?>" method="POST" id="<?php echo $cmb_options->cmb->cmb_id; ?>" enctype="multipart/form-data" encoding="multipart/form-data">
@@ -119,24 +129,19 @@ function yourprefix_options_display_with_tabs( $cmb_options ) {
  * Gets navigation tabs array for CMB2 options pages which share the given
  * display_cb param.
  *
- * @param  mixed  $display_cb The display_cb CMB2 property.
+ * @param CMB2_Options_Hookup $cmb_options The CMB2_Options_Hookup object.
  *
- * @return array              Array of tab information.
+ * @return array Array of tab information.
  */
-function yourprefix_options_page_tabs( $display_cb ) {
-	// $boxes = CMB2_Boxes::get_by( 'display_cb', $display_cb )
-	$boxes = CMB2_Boxes::get_all();
-	$tabs  = array();
+function yourprefix_options_page_tabs( $cmb_options ) {
+	$tab_group = $cmb_options->cmb->prop( 'tab_group' );
+	$tabs      = array();
 
-	foreach ( $boxes as $cmb_id => $cmb ) {
-		if ( $display_cb === $cmb->prop( 'display_cb' ) ) {
-
-			$option_key = $cmb->options_page_keys()[0];
-
-			$tabs[ $option_key ] = array(
-				'class' => 'nav-tab' . ( isset( $_GET['page'] ) && $option_key === $_GET['page'] ? ' nav-tab-active' : '' ),
-				'title' => $cmb->prop( 'title' ),
-			);
+	foreach ( CMB2_Boxes::get_all() as $cmb_id => $cmb ) {
+		if ( $tab_group === $cmb->prop( 'tab_group' ) ) {
+			$tabs[ $cmb->options_page_keys()[0] ] = $cmb->prop( 'tab_title' )
+				? $cmb->prop( 'tab_title' )
+				: $cmb->prop( 'title' );
 		}
 	}
 
