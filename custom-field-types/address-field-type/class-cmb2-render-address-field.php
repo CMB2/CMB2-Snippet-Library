199da9d0b7c4f521f0eb4626f421a19de1635326
Michael Beckwith <tw2113@gmail.com>
2018-10-19 20:33:27 -0500

    a little bit of code cleanup

--- custom-field-types/address-field-type/class-cmb2-render-address-field.php
+++ custom-field-types/address-field-type/class-cmb2-render-address-field.php
@@ -10,7 +10,7 @@ class CMB2_Render_Address_Field extends CMB2_Type_Base {
 	 *
 	 * @var array
 	 */
-	protected static $state_list = array( 'AL'=>'Alabama','AK'=>'Alaska','AZ'=>'Arizona','AR'=>'Arkansas','CA'=>'California','CO'=>'Colorado','CT'=>'Connecticut','DE'=>'Delaware','DC'=>'District Of Columbia','FL'=>'Florida','GA'=>'Georgia','HI'=>'Hawaii','ID'=>'Idaho','IL'=>'Illinois','IN'=>'Indiana','IA'=>'Iowa','KS'=>'Kansas','KY'=>'Kentucky','LA'=>'Louisiana','ME'=>'Maine','MD'=>'Maryland','MA'=>'Massachusetts','MI'=>'Michigan','MN'=>'Minnesota','MS'=>'Mississippi','MO'=>'Missouri','MT'=>'Montana','NE'=>'Nebraska','NV'=>'Nevada','NH'=>'New Hampshire','NJ'=>'New Jersey','NM'=>'New Mexico','NY'=>'New York','NC'=>'North Carolina','ND'=>'North Dakota','OH'=>'Ohio','OK'=>'Oklahoma','OR'=>'Oregon','PA'=>'Pennsylvania','RI'=>'Rhode Island','SC'=>'South Carolina','SD'=>'South Dakota','TN'=>'Tennessee','TX'=>'Texas','UT'=>'Utah','VT'=>'Vermont','VA'=>'Virginia','WA'=>'Washington','WV'=>'West Virginia','WI'=>'Wisconsin','WY'=>'Wyoming' );
+	protected static $state_list = array( 'AL' => 'Alabama', 'AK' => 'Alaska', 'AZ' => 'Arizona', 'AR' => 'Arkansas', 'CA' => 'California', 'CO' => 'Colorado', 'CT' => 'Connecticut', 'DE' => 'Delaware', 'DC' => 'District Of Columbia', 'FL' => 'Florida', 'GA' => 'Georgia', 'HI' => 'Hawaii', 'ID' => 'Idaho', 'IL' => 'Illinois', 'IN' => 'Indiana', 'IA' => 'Iowa', 'KS' => 'Kansas', 'KY' => 'Kentucky', 'LA' => 'Louisiana', 'ME' => 'Maine', 'MD' => 'Maryland', 'MA' => 'Massachusetts', 'MI' => 'Michigan', 'MN' => 'Minnesota', 'MS' => 'Mississippi', 'MO' => 'Missouri', 'MT' => 'Montana', 'NE' => 'Nebraska', 'NV' => 'Nevada', 'NH' => 'New Hampshire', 'NJ' => 'New Jersey', 'NM' => 'New Mexico', 'NY' => 'New York', 'NC' => 'North Carolina', 'ND' => 'North Dakota', 'OH' => 'Ohio', 'OK' => 'Oklahoma', 'OR' => 'Oregon', 'PA' => 'Pennsylvania', 'RI' => 'Rhode Island', 'SC' => 'South Carolina', 'SD' => 'South Dakota', 'TN' => 'Tennessee', 'TX' => 'Texas', 'UT' => 'Utah', 'VT' => 'Vermont', 'VA' => 'Virginia', 'WA' => 'Washington', 'WV' => 'West Virginia', 'WI' => 'Wisconsin', 'WY' => 'Wyoming' );
 
 	public static function init() {
 		add_filter( 'cmb2_render_class_address', array( __CLASS__, 'class_name' ) );
@@ -18,7 +18,7 @@ class CMB2_Render_Address_Field extends CMB2_Type_Base {
 
 		/**
 		 * The following snippets are required for allowing the address field
-		 * to work as a repeatable field, or in a repeatable group
+		 * to work as a repeatable field, or in a repeatable group.
 		 */
 		add_filter( 'cmb2_sanitize_address', array( __CLASS__, 'sanitize' ), 10, 5 );
 		add_filter( 'cmb2_types_esc_address', array( __CLASS__, 'escape' ), 10, 4 );
@@ -48,12 +48,12 @@ class CMB2_Render_Address_Field extends CMB2_Type_Base {
 				$state_list = self::$state_list;
 			}
 
-			// Add the "label" option. Can override via the field text param
+			// Add the "label" option. Can override via the field text param.
 			$state_list = array( '' => esc_html( $this->_text( 'address_select_state_text', 'Select a State' ) ) ) + $state_list;
 
 			$state_options = '';
 			foreach ( $state_list as $abrev => $state ) {
-				$state_options .= '<option value="'. $abrev .'" '. selected( $value['state'], $abrev, false ) .'>'. $state .'</option>';
+				$state_options .= '<option value="' . $abrev . '" ' . selected( $value['state'], $abrev, false ) . '>' . $state . '</option>';
 			}
 		}
 
@@ -63,7 +63,7 @@ class CMB2_Render_Address_Field extends CMB2_Type_Base {
 		}
 
 		ob_start();
-		// Do html
+		// Do html.
 		?>
 		<div><p><label for="<?php echo $this->_id( '_address_1', false ); ?>"><?php echo esc_html( $this->_text( 'address_address_1_text', 'Address 1' ) ); ?></label></p>
 			<?php echo $this->types->input( array(
@@ -100,7 +100,7 @@ class CMB2_Render_Address_Field extends CMB2_Type_Base {
 						'value' => $value['state'],
 						'desc'  => '',
 					) ); ?>
-				<?php else: ?>
+				<?php else : ?>
 					<?php echo $this->types->select( array(
 						'name'    => $this->_name( '[state]' ),
 						'id'      => $this->_id( '_state' ),
@@ -144,7 +144,7 @@ class CMB2_Render_Address_Field extends CMB2_Type_Base {
 	 */
 	public static function maybe_save_split_values( $override_value, $value, $object_id, $field_args ) {
 		if ( ! isset( $field_args['split_values'] ) || ! $field_args['split_values'] ) {
-			// Don't do the override
+			// Don't do the override.
 			return $override_value;
 		}
 
@@ -152,13 +152,13 @@ class CMB2_Render_Address_Field extends CMB2_Type_Base {
 
 		foreach ( $address_keys as $key ) {
 			if ( ! empty( $value[ $key ] ) ) {
-				update_post_meta( $object_id, $field_args['id'] . 'addr_'. $key, sanitize_text_field( $value[ $key ] ) );
+				update_post_meta( $object_id, $field_args['id'] . 'addr_' . $key, sanitize_text_field( $value[ $key ] ) );
 			}
 		}
 
 		remove_filter( 'cmb2_sanitize_address', array( __CLASS__, 'sanitize' ), 10, 5 );
 
-		// Tell CMB2 we already did the update
+		// Tell CMB2 we already did the update.
 		return true;
 	}
 
@@ -173,7 +173,7 @@ class CMB2_Render_Address_Field extends CMB2_Type_Base {
 			$meta_value[ $key ] = array_filter( array_map( 'sanitize_text_field', $val ) );
 		}
 
-		return array_filter($meta_value);
+		return array_filter( $meta_value );
 	}
 
 	public static function escape( $check, $meta_value, $field_args, $field_object ) {
@@ -186,7 +186,7 @@ class CMB2_Render_Address_Field extends CMB2_Type_Base {
 			$meta_value[ $key ] = array_filter( array_map( 'esc_attr', $val ) );
 		}
 
-		return array_filter($meta_value);
+		return array_filter( $meta_value );
 	}
 
 	public function get_split_meta_value( $data, $object_id, $field_args, $field ) {
