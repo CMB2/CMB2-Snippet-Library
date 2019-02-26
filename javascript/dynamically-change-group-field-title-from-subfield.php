e64dcf80cea573796bb2d65022bbe6cb06402726
Justin Sternberg <justin@dsgnwrks.pro>
2019-02-26 12:48:00 -0500

    Fire event on cmb2_remove_row as well.
    See: https://wordpress.org/support/topic/cmb2_before_form-related-question/#post-11249943

--- javascript/dynamically-change-group-field-title-from-subfield.php
+++ javascript/dynamically-change-group-field-title-from-subfield.php
@@ -87,7 +87,7 @@ function yourprefix_add_js_for_repeatable_titles_to_footer() {
 		};
 
 		$box
-			.on( 'cmb2_add_row cmb2_shift_rows_complete', replaceTitles )
+			.on( 'cmb2_add_row cmb2_remove_row cmb2_shift_rows_complete', replaceTitles )
 			.on( 'keyup', replaceOnKeyUp );
 
 		replaceTitles();
